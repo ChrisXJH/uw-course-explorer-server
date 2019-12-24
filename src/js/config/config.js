051466dff15ae283844fe7fa2efa6f76129d428a
Chris Xu <gzchrisxjh@gmail.com>
2019-12-24 00:15:40 -0500

    Fix port config

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT | 8000;
+const port = process.env.PORT || 8000;
 const uwApiKey = process.env.UW_API_KEY;
 const uwBackendUrl = process.env.UW_API_URL;
 const env = process.env.NODE_ENV;
