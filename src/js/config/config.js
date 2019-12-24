3a24477d90b6e36df5d75b8db35e9f24c032c359
Chris Xu <gzchrisxjh@gmail.com>
2019-12-24 00:00:24 -0500

    Add dotenv

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,7 +1,11 @@
-const port = 8000;
-const uwApiKey = '';
-const uwBackendUrl = 'https://api.uwaterloo.ca/v2';
-const env = 'dev';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT | 8000;
+const uwApiKey = process.env.UW_API_KEY;
+const uwBackendUrl = process.env.UW_API_URL;
+const env = process.env.NODE_ENV;
 
 export function getEnv() {
   return env;
